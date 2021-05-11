3c34609f5e88c704770fe4bbb1f02e1a2c57f1c2
AISoftMaster <ormonkenenbaev@gmail.com>
2021-05-11 19:47:13 +0600

    uploads

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth import get_user_model
 
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), related_name='profile', on_delete=models.CASCADE, verbose_name='Пользователь')
-    avatar = models.ImageField(null=True, blank=True, upload_to='#', verbose_name='Аватар')
+    avatar = models.ImageField(null=True, blank=True, upload_to='user_pics', verbose_name='Аватар')
     curriculum = models.CharField(max_length=600, null=True, blank=True)
 
     class Meta:
